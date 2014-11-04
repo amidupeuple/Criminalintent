78cf1f3aecd2769b2cfa95caf9bd12c599e3203e
amidupeuple <danyapivovarov@gmail.com>
2014-11-05 00:15:03 +0300

    Initial commit

---
+++ src/com/example/criminalintent/CrimeActivity.java
@@ -0,0 +1,22 @@
+package com.example.criminalintent;
+
+import java.util.UUID;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CrimeActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment() {
+		UUID crimeId = (UUID) getIntent().getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+		
+		return CrimeFragment.newInstance(crimeId);
+	}
+	
+}
